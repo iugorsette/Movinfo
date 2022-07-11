f069fbc3dc07629ef15e0d43bbe1992b66cdceac
Masso <luizmiguelrosalazarinirubio@gmail.com>
2022-07-11 14:01:04 -0300

    adicionando a lógica de mais algumas funções

--- js/criar.js
+++ js/criar.js
@@ -7,19 +7,28 @@ function criarFilmesLancamentos(filmes){
             filmesLancamentos[filmesLancamentos.length]=filme //ele esta adicionando o filme na lista dos filmes lançamentos
         }
     }
+    filmesLancamentos.sort()//ordenados dos mais recentes 
 }
 
-function criarFilmesRecomendados(filmes){ 
+function criarFilmesRecomendados(filmes, usuario){ 
+    let personalidadesDoUsuario = usuario.personalidades //Pegar a personalidade do usuario   
 
 }
 
 function criarFilmesMaisAmados(filmes){
-
+    let filmesMaisAmados = [] // lista de filmes mais amados
+    for (filme of filmes) {  // percorre a lista de filmes
+        if (filme.mediaAvaliacao >4){  // verifica a media de avaliação
+            filmesMaisAmados[filmesMaisAmados.length]=filme //adiciona o filme na lista de filmes mais amados
+        }
+    }
+    filmesMaisAmados.sort() //ordena os filmes dos mais bem avaliados
 }
 
 function criarFilmesMaisAclamados(filmes){
 
 }
-function criarFilmesRecordeBilheteria(filmes){
+function criarFilmesRecordeBilheteria(filmes){ 
+    let recordesDeBilheteria = filmes.sort() // ordenados dos filmes q tiveram mais bilheteria 
 
 }
